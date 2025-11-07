08d19553349853fcb2917aaa4eb300bd5df82ef6
Toni Dzwoniarski <tonidzwoniarski@mbp-von-toni.speedport.ip>
2025-11-07 20:57:57 +0100

    NLP Analysis

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+# import modules ---------------------------------------------------------
 import pandas as pd
 
-df = pd.read_csv("costumer_complaints_data/consumer_complaints.csv")
-print(df.shape)
-print(df.columns)
-df.head()
+
+# main code --------------------------------------------------------------
+df = pd.read_csv("costumer_complain_data/consumer_complaints.csv")
+
+# check the data file for values and show the first five rows 
+print("\n",df.shape,"\n")
+print(df.columns,"\n")
+print(df.head(5))
+
+df.isnull().sum()
+df = df.dropna(subset=["consumer_complaint_narrative"])
+print("\n",df.shape,"\n")
+
+df = df.drop_duplicates()
+print("\n",df.shape,"\n")
