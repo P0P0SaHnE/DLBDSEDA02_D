537ac6d68cc20d0a4560939a6367d5fb5280c1b6
Toni Dzwoniarski <tonidzwoniarski@MacBook-Pro-von-Toni.local>
2025-11-13 23:27:41 +0100

    NLP Analyse

--- main.py
+++ main.py
@@ -1,47 +1,58 @@
 # MODULES ----------------------------------------------------------------------------------------------------------------------
 
 import pandas as pd
+import csv
 import re
 import nltk
 import spacy
 from nltk.corpus import stopwords
-
-
-from tqdm import tqdm
+from tqdm import tqdm   #
 tqdm.pandas()
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 
 # VARIABLES --------------------------------------------------------------------------------------------------------------------
 
-corpus_dataframe = pd.read_csv("costumer_complain_data/consumer_complaints.csv") # define the datafile
-complaint_narrative = "consumer_complaint_narrative" # define columne in the dataframe that is important
-relevant_columns = ["product", "issue","sub_issue", "consumer_complaint_narrative"] # define all relevant columns in the dataframe
+corpus_dataframe = pd.read_csv(     
+    "costumer_complain_data/consumer_complaints.csv",   # define the datafile
+    sep=",",                 # choose seperator
+    quotechar='"',           # character start and end of quoted field
+    engine="python",         # python parser
+    on_bad_lines="warn",     # warn at bad line interpretation
+)
+
+complaint_narrative = "consumer_complaint_narrative"    # define columne in the dataframe that is important
+relevant_columns = ["product", "issue","sub_issue", "consumer_complaint_narrative"]     # define all relevant columns in the dataframe
 try:
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(stopwords.words('english'))    # download if stopwords not found
 except LookupError:
     nltk.download('stopwords')
     stop_words = set(stopwords.words('english'))
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")  # load the language modell for "english"
 
 
-# DEFINITIONS ------------------------------------------------------------------------------------------------------------------
+# FUNCTIONS ------------------------------------------------------------------------------------------------------------------
 
 def clean_text(text):
-    text = text.lower()
-    text = text.replace('xxxx', '').replace('xx', '') # remove xxxx from vocabs
+# text preparation and removing of stopwords 
+
+    text = text.lower() # all letter in lower
+    text = text.replace('xxxx', '').replace('xx', '') # remove xxx from text
     text = re.sub(r'[^a-z\s]', '', text).strip()  # replace everything that is not letter from a - z or blankspace with nothing
 
     words = text.split()
-    cleaned_words = []
+    cleaned_words = []  # empty list
+
     for word in words:
         if word not in stop_words:
             cleaned_words.append(word)
-    text = ' '.join(cleaned_words)
+    text = ' '.join(cleaned_words)  # join the single words into text with black space between
 
     return text
 
 
 def lemmatize_text(text):
+# lemmatization
     lemmatized_tokens = []
     doc = nlp(text)
     for token in doc:
@@ -55,14 +66,16 @@ def lemmatize_text(text):
 
 
 def build_final_text(row):
-    extras = ' '.join(str(x).lower() for x in [row["product"], row["issue"], row["sub_issue"]] if pd.notna(x))
+    extras = ' '.join(
+        str(x).lower() for x in [row["product"], row["issue"], row["sub_issue"]] if pd.notna(x)
+    )
     return extras + " " + row["lemma_text"]
 
 
 # MAIN -------------------------------------------------------------------------------------------------------------------------
 
 print("\n--------------------------------------------------------------------------------------------------------")
-print("##  This is the Project for the IU Modul DLBDSEDA02_D with the topic of Data Analysis.  ##")
+print("##         This is the Project for the IU Modul DLBDSEDA02_D with the topic of Data Analysis.         ##")
 print("--------------------------------------------------------------------------------------------------------\n")
 print(f"\nThe used Dataframe have {corpus_dataframe.shape[0]} rows and {corpus_dataframe.shape[1]} columns.\n")
 
@@ -72,10 +85,9 @@ print(f"\nThe used Dataframe have {corpus_dataframe.shape[0]} rows and {corpus_d
 print("\n> Phase 1: Prepare the Dataframe <")
 print("----------------------------------\n")
 
-corpus_dataframe = corpus_dataframe[relevant_columns]
-corpus_dataframe = corpus_dataframe.dropna(subset=[complaint_narrative])
-corpus_dataframe = corpus_dataframe.drop_duplicates()
-
+corpus_dataframe = corpus_dataframe[relevant_columns]   # remove all unuser columns
+corpus_dataframe = corpus_dataframe.dropna(subset=[complaint_narrative])    # remove all rows with empty field in complaint_narrative
+corpus_dataframe = corpus_dataframe.drop_duplicates()   # remove duplicate rows
 
 print(f"\nThe Dataframe have {corpus_dataframe.shape[0]} rows and {corpus_dataframe.shape[1]} columns after the preparation.\n")
 
@@ -85,14 +97,15 @@ print(f"\nThe Dataframe have {corpus_dataframe.shape[0]} rows and {corpus_datafr
 print("\n> Phase 2: Text Cleaning <")
 print("--------------------------\n")
 
-corpus_dataframe["clean_text"] = corpus_dataframe[complaint_narrative].progress_apply(clean_text)
+corpus_dataframe["clean_text"] = corpus_dataframe[complaint_narrative].progress_apply(clean_text) 
 
-print(corpus_dataframe.sample(10, random_state=42),"\n")
+print("\nExample of 10 rows after Text Cleaning:\n")
+print(corpus_dataframe.sample(20, random_state=42),"\n")
 
 # tokenizing, lemmatizing -------------------------------------------------------------------------------------------------------
 
 print("\n> Phase 3: Tokenizing & Lemmatize <")
-print("--------------------------\n")
+print("-----------------------------------\n")
 
 # try to skip the lammatize process with existing lemma file
 try:
@@ -100,11 +113,32 @@ try:
 
 except:
     corpus_dataframe["lemma_text"] = corpus_dataframe["clean_text"].progress_apply(lemmatize_text)
-    corpus_dataframe.to_csv("lemma_text.csv", index=False)
+    corpus_dataframe.to_csv(
+    "lemma_text.csv",
+    index=False,
+    quoting=csv.QUOTE_ALL,
+    lineterminator="\n"
+)
 
 print(corpus_dataframe.sample(10, random_state=42),"\n")
 
 corpus_dataframe["final_text"] = corpus_dataframe.progress_apply(build_final_text, axis=1)
 
-corpus_dataframe.to_csv("cleaned_complaints.csv", index=False)
+# bow vectorization -------------------------------------------------------------------------------------------------------------
+
+texts = corpus_dataframe["final_text"]  # oder "lemma_text", wenn du Punkt 4 nicht machst
+
+bow_vectorizer = CountVectorizer(
+    max_df=0.9,
+    min_df=5,
+    ngram_range=(1,1)
+)
+X_bow = bow_vectorizer.fit_transform(texts)
+
+tfidf_vectorizer = TfidfVectorizer(
+    max_df=0.9,
+    min_df=5,
+    ngram_range=(1,1)
+)
+X_tfidf = tfidf_vectorizer.fit_transform(texts)
 
